174b615efbb54e88eec51a3212cd2651eb5929fb
MentalSep <chahidjava@hotmail.com>
2024-04-18 11:34:33 +0100

    add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the combined string."""
+    return str1 + str2
