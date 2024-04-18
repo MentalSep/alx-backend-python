72123c661a099af383b92e9051f857e781e796a6
MentalSep <chahidjava@hotmail.com>
2024-04-18 11:42:43 +0100

    add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Calculates the sum of elements in a list of floats."""
+    return sum(input_list)
