20501047d92ab6a50eeacabfa482e9fe0e9761bb
MentalSep <chahidjava@hotmail.com>
2024-04-22 15:37:45 +0100

    typo

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay between 0 and max_delay and returns it."""
-    delay = asyncio.uniform(0, max_delay)
-    await random.sleep(delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
