b0acc9ead2704a3887877aff2e97bb3573d48707
MentalSep <chahidjava@hotmail.com>
2024-04-22 15:35:12 +0100

    add task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Model for task 0"""
-from asyncio import sleep
-from random import uniform
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay between 0 and max_delay and returns it."""
-    delay = uniform(0, max_delay)
-    await sleep(delay)
+    delay = asyncio.uniform(0, max_delay)
+    await random.sleep(delay)
     return delay
