c1a79560c12897e4f1c162fe97eb67409a968451
MentalSep <chahidjava@hotmail.com>
2024-04-23 15:58:46 +0100

    add type annotation for both files

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """Module that defines an async generator"""
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronously generates random numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Module """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Asynchronously collects 10 random numbers using async comprehension."""
     return [x async for x in async_generator()]
