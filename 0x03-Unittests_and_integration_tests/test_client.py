42d845dfc5d0a3e0081f6e245b618bc710273ad8
MentalSep <chahidjava@hotmail.com>
2024-05-12 10:54:54 +0100

    add task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Tests for client.py"""
+import unittest
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for GithubOrgClient."""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test org."""
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == '__main__':
+    unittest.main()
