66f0cda2e53fbc9eb2f36de883829ee5f74ecad2
MentalSep <chahidjava@hotmail.com>
2024-05-12 11:29:36 +0100

    add task 7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,6 +42,15 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(client.public_repos("b-key"), ["b"])
             self.assertEqual(client.public_repos(), ["a", "b"])
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license."""
+        self.assertEqual(GithubOrgClient.has_license(repo, license_key),
+                         expected)
+
 
 if __name__ == '__main__':
     unittest.main()
