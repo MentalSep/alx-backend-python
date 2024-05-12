a19509119e42c2a75e9325910f322d783f515396
MentalSep <chahidjava@hotmail.com>
2024-05-12 11:49:38 +0100

    add task 9

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -84,6 +84,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(repos, self.expected_repos)
 
+    def test_public_repos_with_license(self):
+        """Test public_repos with license"""
+        github_org_client = GithubOrgClient('testorg')
+
+        repos = github_org_client.public_repos("apache-2.0")
+
+        self.assertEqual(repos, self.apache2_repos)
+
 
 if __name__ == '__main__':
     unittest.main()
